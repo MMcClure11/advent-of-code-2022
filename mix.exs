2d0ea5a36312570101a4ecf5da6a8c7ade05edeb
MMcClure11 <mmcclure0100@gmail.com>
2022-12-01 19:07:08 -0600

    Add Dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule AdventOfCode.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_apps: [:mix, :ex_unit],
+        ignore_warnings: ".dialyzer_ignore.exs",
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
+        check_plt: true
+      ]
     ]
   end
 
@@ -23,6 +29,7 @@ defmodule AdventOfCode.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
     ]
   end
 end
